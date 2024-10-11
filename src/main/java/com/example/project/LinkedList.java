85e55170fe87673b3e19d56c3be7bbb56389eb70
IPSUL <160661933+IPSUL@users.noreply.github.com>
2024-10-11 18:38:43 +0300

    Update LinkedList.java

--- src/main/java/com/example/project/LinkedList.java
+++ src/main/java/com/example/project/LinkedList.java
@@ -56,11 +56,33 @@ public class LinkedList<T> implements List<T>{
     }
     public T mostFrequentElement() {
         throw new UnsupportedOperationException("Not supported yet.");
-        //         Write the method mostFrequentElement, member of the class LinkedList, that returns
-        // the most frequent element in the list. The most frequent element is the element
-        // appearing the highest number of times. If one or more element appear the same
-        // number of times, the one encountered earlier is returned.
-        // Example 1.1. Given the list l : A, B, C, B, C, D, E, mostFrequentElement() returns
-        // B.
+    if (empty()) {
+        return null;
+    }
+
+    Node<T> tmp = head;
+    T mostFrequent = null;
+    int maxCount = 0;
+
+    while (tmp != null) {
+        Node<T> checker = head;
+        int count = 0;
+
+        while (checker != null) {
+            if (tmp.data.equals(checker.data)) {
+                count++;
+            }
+            checker = checker.next;
+        }
+
+        if (count > maxCount) {
+            maxCount = count;
+            mostFrequent = tmp.data;
+        }
+
+        tmp = tmp.next;
+    }
+
+    return mostFrequent;
     }
 }
